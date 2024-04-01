d7d91d5987bf8bec53de8827e9d38fa9b86cb393
hossein-moghadam <hosseinghmoghadam@gmail.com>
2024-04-02 03:20:10 +0330

    chore: get details

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gocolly/colly"
 	ptime "github.com/yaa110/go-persian-calendar"
 	"go-enamad-test/config"
@@ -46,6 +47,11 @@ func GetCompanyPage(index int) {
 		var newCompany models.Company
 
 		database.Connection().Create(&company).Scan(&newCompany)
+
+		if m["code"][0] != "" {
+			go GetDetails(m["code"][0], m["id"][0], company.ID)
+		}
+
 		//companies = append(companies, company)
 	})
 
@@ -79,6 +85,29 @@ func StrToTime(srtDate string) (time.Time, error) {
 	)
 
 	return pt.Time(), nil
+}
+
+func GetDetails(code string, id string, compamyID uint) {
+	c := colly.NewCollector()
+
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("referer", "https://enamad.ir/")
+	})
+
+	c.OnHTML("div.row.person_details > div.mainul", func(e *colly.HTMLElement) {
+
+		var company models.Company
+
+		database.Connection().First(&company, compamyID)
+		company.Address = strings.TrimSpace(e.ChildText("div.row:nth-child(1) > div.col-sm-12.col-md-8.contentinformation.licontent.mobiledes"))
+		company.Phone = strings.TrimSpace(e.ChildText("div.row:nth-child(2) > div.col-sm-12.col-md-8.contentinformation.licontent.mobiledes"))
+		company.Email = strings.TrimSpace(e.ChildText("div.row:nth-child(3) > div.col-sm-12.col-md-8.contentinformation.licontent.mobiledes"))
+		company.AnswerTime = strings.TrimSpace(e.ChildText("div.row:nth-child(4) > div.col-sm-12.col-md-8.contentinformation.licontent.mobiledes"))
+		database.Connection().Save(&company)
+	})
+
+	c.Visit(fmt.Sprintf("https://trustseal.enamad.ir/?id=%s&code=%s", id, code))
+	c.Wait()
 }
 
 func main() {
--- models/company_model.go
+++ models/company_model.go
@@ -10,6 +10,11 @@ type Company struct {
 	Domain     string `gorm:"varchar:191"`
 	Name       string `gorm:"varchar:191"`
 	State      string `gorm:"varchar:191"`
+	EnamadID   string `gorm:"varchar:191"`
+	Address    string `gorm:"varchar:191"`
+	Phone      string `gorm:"varchar:191"`
+	Email      string `gorm:"varchar:191"`
+	AnswerTime string `gorm:"varchar:191"`
 	City       string `gorm:"varchar:191"`
 	CreateDate time.Time
 	ExpiryDate time.Time
